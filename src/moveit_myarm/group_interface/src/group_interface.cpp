3f7942d91ea8f9f555a3261f342490502127fa8d
shuiqingxd <gxdhqn@126.com>
2018-05-03 17:49:11 +0800

    5.03.17:48revised group_interface

--- src/moveit_myarm/group_interface/src/group_interface.cpp
+++ src/moveit_myarm/group_interface/src/group_interface.cpp
@@ -52,6 +52,10 @@ int main(int argc, char **argv)
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
+  ros::WallDuration sleep_time(5.0);
+  sleep_time.sleep();
+
+
   // BEGIN_TUTORIAL
   //
   // Setup
@@ -113,14 +117,10 @@ int main(int argc, char **argv)
   target_pose1.orientation.x = 0.0;
   target_pose1.orientation.y = 0.0;
   target_pose1.orientation.z = 1.0;
-  target_pose1.position.x = 0;
-  target_pose1.position.y = -0.2;
+  target_pose1.position.x = 0.2;
+  target_pose1.position.y = -0;
   target_pose1.position.z = -0.8;
   move_group.setPoseTarget(target_pose1);
-
-  ros::WallDuration sleep_time(5.0);
-  sleep_time.sleep();
-
 
   // Now, we call the planner to compute the plan and visualize it.
   // Note that we are just planning, not asking move_group
@@ -216,9 +216,9 @@ int main(int argc, char **argv)
   start_pose2.orientation.x = 0.0;
   start_pose2.orientation.y = 0.0;
   start_pose2.orientation.z = 1.0;
-  start_pose2.position.x = 0;
-  start_pose2.position.y = -0.2;
-  start_pose2.position.z = -0.8;
+  start_pose2.position.x = 0.2;
+  start_pose2.position.y = -0;
+  start_pose2.position.z = -1.0;
   start_state.setFromIK(joint_model_group, start_pose2);
   move_group.setStartState(start_state);
 
@@ -251,6 +251,7 @@ int main(int argc, char **argv)
   // for the end-effector to go through. Note that we are starting
   // from the new start state above.  The initial pose (start state) does not
   // need to be added to the waypoint list but adding it can help with visualizations
+  
   std::vector<geometry_msgs::Pose> waypoints;
 
   geometry_msgs::Pose start_pose3;
@@ -262,11 +263,15 @@ int main(int argc, char **argv)
   start_pose3.position.y = -0;
   start_pose3.position.z = -1.057;
 
+  start_state.setFromIK(joint_model_group, start_pose3);
+  move_group.setStartState(start_state);
+
+
   waypoints.push_back(start_pose3);
 
   geometry_msgs::Pose target_pose3 = start_pose3;
 
-  target_pose3.position.z += 0.2;
+  target_pose3.position.z += 0.205;
   waypoints.push_back(target_pose3);  // up 
 
   //target_pose3.position.y -= 0.1;
@@ -277,7 +282,7 @@ int main(int argc, char **argv)
   target_pose3.position.x -= 0.2;
   waypoints.push_back(target_pose3);  // down and right 
 
-  target_pose3.position.z -= 0.2;
+  target_pose3.position.z -= 0.15;
   waypoints.push_back(target_pose3);  // down and right 
   target_pose3.position.x += 0.2;
   waypoints.push_back(target_pose3);  // down and right 
@@ -309,98 +314,121 @@ int main(int argc, char **argv)
   visual_tools.trigger();
   visual_tools.prompt("next step");
 
-//  // Adding/Removing Objects and Attaching/Detaching Objects
-//  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-//  //
-//  // Define a collision object ROS message.
-//  moveit_msgs::CollisionObject collision_object;
-//  collision_object.header.frame_id = move_group.getPlanningFrame();
-//
-//  // The id of the object is used to identify it.
-//  collision_object.id = "box1";
-//
-//  // Define a box to add to the world.
-//  shape_msgs::SolidPrimitive primitive;
-//  primitive.type = primitive.BOX;
-//  primitive.dimensions.resize(3);
-//  primitive.dimensions[0] = 0.4;
-//  primitive.dimensions[1] = 0.1;
-//  primitive.dimensions[2] = 0.4;
-//
-//  //Define a pose for the box (specified relative to frame_id)
-//  geometry_msgs::Pose box_pose;
-//  box_pose.orientation.w = 1.0;
-//  box_pose.position.x = 0.6;
-//  box_pose.position.y = -0.4;
-//  box_pose.position.z = 1.2;
-//
-//  collision_object.primitives.push_back(primitive);
-//  collision_object.primitive_poses.push_back(box_pose);
-//  collision_object.operation = collision_object.ADD;
-//
-//  std::vector<moveit_msgs::CollisionObject> collision_objects;
-//  collision_objects.push_back(collision_object);
-//
-//  // Now, let's add the collision object into the world
-//  ROS_INFO_NAMED("tutorial", "Add an object into the world");
-//  planning_scene_interface.addCollisionObjects(collision_objects);
-//
-//  // Show text in Rviz of status
-//  visual_tools.publishText(text_pose, "Add object", rvt::WHITE, rvt::XLARGE);
-//  visual_tools.trigger();
-//
-//  // Sleep to allow MoveGroup to recieve and process the collision object message
-//  ros::Duration(1.0).sleep();
-//
-//  // Now when we plan a trajectory it will avoid the obstacle
-//  move_group.setStartState(*move_group.getCurrentState());
-//  move_group.setPoseTarget(target_pose1);
-//
-//  success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-//  ROS_INFO_NAMED("tutorial", "Visualizing plan 5 (pose goal move around cuboid) %s", success ? "" : "FAILED");
-//
-//  // Visualize the plan in Rviz
-//  visual_tools.deleteAllMarkers();
-//  visual_tools.publishText(text_pose, "Obstacle Goal", rvt::WHITE, rvt::XLARGE);
-//  visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
-//  visual_tools.trigger();
-//  visual_tools.prompt("next step");
-//
-//  // Now, let's attach the collision object to the robot.
-//  ROS_INFO_NAMED("tutorial", "Attach the object to the robot");
-//  move_group.attachObject(collision_object.id);
-//
-//  // Show text in Rviz of status
-//  visual_tools.publishText(text_pose, "Object attached to robot", rvt::WHITE, rvt::XLARGE);
-//  visual_tools.trigger();
-//
-//  /* Sleep to allow MoveGroup to recieve and process the attached collision object message */
-//  ros::Duration(1.0).sleep();
-//
-//  // Now, let's detach the collision object from the robot.
-//  ROS_INFO_NAMED("tutorial", "Detach the object from the robot");
-//  move_group.detachObject(collision_object.id);
-//
-//  // Show text in Rviz of status
-//  visual_tools.publishText(text_pose, "Object dettached from robot", rvt::WHITE, rvt::XLARGE);
-//  visual_tools.trigger();
-//
-//  /* Sleep to allow MoveGroup to recieve and process the detach collision object message */
-//  ros::Duration(1.0).sleep();
-//
-//  // Now, let's remove the collision object from the world.
-//  ROS_INFO_NAMED("tutorial", "Remove the object from the world");
-//  std::vector<std::string> object_ids;
-//  object_ids.push_back(collision_object.id);
-//  planning_scene_interface.removeCollisionObjects(object_ids);
-//
-//  // Show text in Rviz of status
-//  visual_tools.publishText(text_pose, "Object removed", rvt::WHITE, rvt::XLARGE);
-//  visual_tools.trigger();
-//
-//  /* Sleep to give Rviz time to show the object is no longer there.*/
-//  ros::Duration(1.0).sleep();
-//
+  // Adding/Removing Objects and Attaching/Detaching Objects
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  //
+  // Define a collision object ROS message.
+  moveit_msgs::CollisionObject collision_object;
+  collision_object.header.frame_id = move_group.getPlanningFrame();
+
+  // The id of the object is used to identify it.
+  collision_object.id = "box1";
+
+  // Define a box to add to the world.
+  shape_msgs::SolidPrimitive primitive;
+  primitive.type = primitive.BOX;
+  primitive.dimensions.resize(3);
+  primitive.dimensions[0] = 0.05;
+  primitive.dimensions[1] = 0.4;
+  primitive.dimensions[2] = 0.2;
+
+  //Define a pose for the box (specified relative to frame_id)
+  geometry_msgs::Pose box_pose;
+  box_pose.orientation.w = 1.0;
+//  box_pose.orientation.x = 1.0;
+//  box_pose.orientation.y = 1.0;
+//  box_pose.orientation.z = 1.0;
+  box_pose.position.x = 0.15;
+  box_pose.position.y = -0.0;
+  box_pose.position.z = -1.0;
+
+  collision_object.primitives.push_back(primitive);
+  collision_object.primitive_poses.push_back(box_pose);
+  collision_object.operation = collision_object.ADD;
+
+  std::vector<moveit_msgs::CollisionObject> collision_objects;
+  collision_objects.push_back(collision_object);
+
+  // Now, let's add the collision object into the world
+  ROS_INFO_NAMED("tutorial", "Add an object into the world");
+  planning_scene_interface.addCollisionObjects(collision_objects);
+
+  // Show text in Rviz of status
+  visual_tools.publishText(text_pose, "Add object", rvt::WHITE, rvt::XLARGE);
+  visual_tools.trigger();
+
+  // Sleep to allow MoveGroup to recieve and process the collision object message
+  ros::Duration(1.0).sleep();
+
+  // Now when we plan a trajectory it will avoid the obstacle
+  move_group.setStartState(*move_group.getCurrentState());
+  move_group.setPoseTarget(target_pose1);
+
+  success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  ROS_INFO_NAMED("tutorial", "Visualizing plan 5 (pose goal move around cuboid) %s", success ? "" : "FAILED");
+
+  // Visualize the plan in Rviz
+  visual_tools.deleteAllMarkers();
+  visual_tools.publishText(text_pose, "Obstacle Goal", rvt::WHITE, rvt::XLARGE);
+  visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
+  visual_tools.trigger();
+  visual_tools.prompt("next step");
+
+  // Now, let's attach the collision object to the robot.
+  ROS_INFO_NAMED("tutorial", "Attach the object to the robot");
+  move_group.attachObject(collision_object.id);
+
+  // Show text in Rviz of status
+  visual_tools.publishText(text_pose, "Object attached to robot", rvt::WHITE, rvt::XLARGE);
+  visual_tools.trigger();
+
+  /* Sleep to allow MoveGroup to recieve and process the attached collision object message */
+  ros::Duration(1.0).sleep();
+
+  // Now, let's detach the collision object from the robot.
+  ROS_INFO_NAMED("tutorial", "Detach the object from the robot");
+  move_group.detachObject(collision_object.id);
+
+  // Show text in Rviz of status
+  visual_tools.publishText(text_pose, "Object dettached from robot", rvt::WHITE, rvt::XLARGE);
+  visual_tools.trigger();
+
+  /* Sleep to allow MoveGroup to recieve and process the detach collision object message */
+  ros::Duration(1.0).sleep();
+
+  // Now, let's remove the collision object from the world.
+  ROS_INFO_NAMED("tutorial", "Remove the object from the world");
+  std::vector<std::string> object_ids;
+  object_ids.push_back(collision_object.id);
+  planning_scene_interface.removeCollisionObjects(object_ids);
+
+  // Show text in Rviz of status
+  visual_tools.publishText(text_pose, "Object removed", rvt::WHITE, rvt::XLARGE);
+  visual_tools.trigger();
+
+  /* Sleep to give Rviz time to show the object is no longer there.*/
+  ros::Duration(1.0).sleep();
+
+  visual_tools.trigger();
+  visual_tools.prompt("next step");
+
+
+  // Move to the target again.
+ 
+  move_group.setStartState(*move_group.getCurrentState());
+  move_group.setPoseTarget(target_pose1);
+
+  success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  ROS_INFO_NAMED("tutorial", "Visualizing plan 5 (pose goal move around cuboid) %s", success ? "" : "FAILED");
+
+
+  visual_tools.deleteAllMarkers();
+  visual_tools.publishText(text_pose, "Without Obstacle Goal", rvt::WHITE, rvt::XLARGE);
+  visual_tools.publishAxisLabeled(target_pose1, "goal");
+  visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
+  
+  ros::Duration(5.0).sleep();
+  visual_tools.trigger();
 //  // Dual-arm pose goals
 //  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 //  // First define a new group for addressing the two arms.
